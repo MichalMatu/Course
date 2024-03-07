cfa7ca66a13c4fb520990276d75dc0ed3ce433bf
MichalMatu <michal.matuszewski1984@gmail.com>
2024-03-07 00:37:04 +0000

    a

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
       <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      <Button
+        title="Press me"
+        onPress={() => alert('Button Pressed!')}    
+      />
       <StatusBar style="auto" />
     </View>
   );
